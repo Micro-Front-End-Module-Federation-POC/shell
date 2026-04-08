3809c3b68367576d2a27bfaa1b7a064ea2c676e9
SnehaS2000 <Snehasoundarrajan4610@gmail.com>
2026-04-09 00:59:00 +0530

    updated responsiveness

--- src/app/app.ts
+++ src/app/app.ts
@@ -10,13 +10,21 @@ import { Course, SharedStateService } from '@micro-front-end-module-federation-p
 })
 export class App implements OnInit {
   protected readonly sharedStateValue = signal<number | null>(null);
-  constructor(private sharedState: SharedStateService) {
-  }
+  protected menuOpen = signal(false);
+
+  constructor(private sharedState: SharedStateService) {}
 
   ngOnInit(): void {
-    this.sharedState.state$.subscribe(value => {
+    this.sharedState.state$.subscribe((value: Course[]) => {
       this.sharedStateValue.set(value.length);
     });
   }
 
+  toggleMenu() {
+    this.menuOpen.update(v => !v);
+  }
+
+  closeMenu() {
+    this.menuOpen.set(false);
+  }
 }
